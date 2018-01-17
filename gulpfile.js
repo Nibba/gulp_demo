086ab0442e878d2ab1563f033234397737e9c892
Flam <FullNibba@163.com>
2018-01-18 00:18:25 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,58 +15,6 @@
         gulpJs = require('./gulp/gulp.javascript'),
         gulpImage = require('./gulp/gulp.image');
 
-
-    let handleError = function (err) {
-        let colors = gutil.colors;
-        gutil.log(colors.red("error"));
-        gutil.log('fileName: ' + colors.red(err.fileName));
-        gutil.log('lineNumber: ' + colors.red(err.lineNumber));
-        gutil.log('message: ' + err.message);
-        gutil.log('plugin: ' + colors.yellow(err.plugin));
-    }
-
-    // gulp.task('watchjs', function () {
-    //     gulp.watch(src + static + 'js/**/*.js', function (event) {
-    //         let paths = watchPath(event, src + static + '', dist_static + '')
-    //         let combined = combiner.obj([
-    //             gulp.src(paths.srcPath),
-    //             // sourcemaps.init(),
-    //             uglify(),
-    //             // sourcemaps.write('./'),
-    //             gulp.dest(paths.distDir)
-    //         ])
-    //         combined.on('error', handleError)
-    //     })
-    // })
-
-    // gulp.task('uglifyjs', function () {
-    //     let combined = combiner.obj([
-    //         gulp.src(src + static + 'js/**/*.js'),
-    //         uglify(),
-    //         gulp.dest(dist_static + 'js/')
-    //     ])
-    //     combined.on('error', handleError);
-    // })
-
-    // gulp.task('watchimage', function () {
-    //     gulp.watch(src + static + 'images/**/*', function (event) {
-    //         let paths = watchPath(event, src + static + '', dist_static + '')
-    //         gulp.src(paths.srcPath)
-    //             .pipe(imagemin({
-    //                 progressive: true
-    //             }))
-    //             .pipe(gulp.dest(paths.distDir))
-    //     })
-    // })
-
-    // gulp.task('image', function () {
-    //     gulp.src(src + static + 'images/**/*')
-    //         .pipe(imagemin({
-    //             progressive: true
-    //         }))
-    //         .pipe(gulp.dest(dist_static + '/images'))
-    // })
-
     gulp.task('build:js', () => gulpJs());
 
     gulp.task('build:image', () => gulpImage());
